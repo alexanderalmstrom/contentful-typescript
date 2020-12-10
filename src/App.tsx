bd363a15d31064ee3aa88d1cd2772decb54f94d7
Alexander Almström <alexander.almstrom@panagora.se>
2020-12-11 00:44:57 +0100

    Rename get posts to fetch posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer } from "react";
 import styles from "./App.module.scss";
-import { getPosts } from "./services/posts";
+import { fetchPosts } from "./services/posts";
 import postReducer from "./reducers/postReducer";
 import Posts from "./components/Posts";
 
@@ -8,7 +8,7 @@ const App = () => {
   const [{ posts }, dispatch] = useReducer(postReducer, { posts: [] });
 
   const loadPosts = async () => {
-    dispatch({ type: "LOAD_POSTS", payload: await getPosts() });
+    dispatch({ type: "LOAD_POSTS", payload: await fetchPosts() });
   };
 
   useEffect(() => {
--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,5 +1,5 @@
 import { EntryCollection } from "contentful";
 import contentfulClient from "./contentfulClient";
 
-export const getPosts = async (): Promise<EntryCollection<any>> =>
+export const fetchPosts = async (): Promise<EntryCollection<any>> =>
   contentfulClient.getEntries({ content_type: "post" });
