aa5a702f9df79c32f440406e6029c3054cd0b277
Alexander Almström <alexander.almstrom@panagora.se>
2020-12-12 23:10:16 +0100

    Testin apollo and apollo-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React, { useEffect, useReducer } from "react";
+import { gql, useQuery } from "@apollo/client";
 import styles from "./App.module.scss";
+import { Product } from "./interfaces/Product";
 import { fetchPosts } from "./services/posts";
 import postReducer from "./reducers/postReducer";
 import Posts from "./components/Posts";
 
 const App = () => {
   const [{ posts }, dispatch] = useReducer(postReducer, { posts: [] });
+  const { data } = useQuery(gql`
+    query AllProducts {
+      allProducts {
+        data {
+          _id
+          name
+          price
+        }
+      }
+    }
+  `);
 
   const loadPosts = async () => {
     dispatch({ type: "LOAD_POSTS", payload: await fetchPosts() });
@@ -18,6 +31,14 @@ const App = () => {
   return (
     <div className={styles.root}>
       <Posts posts={posts} />
+      {data?.allProducts.data.map((product: Product) => {
+        return (
+          <div key={product._id}>
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 import "./index.scss";
 import App from "./App";
 
+const cache = new InMemoryCache();
+const token = process.env.REACT_APP_FAUNA_SECRET;
+
+const link = createHttpLink({
+  uri: "https://graphql.fauna.com/graphql",
+  headers: {
+    authorization: `Bearer ${token}`,
+  },
+});
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById("root")
 );
